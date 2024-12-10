5efccbfd353f6ee912c362b43b9e13f12f3ed70b
xevolo <xevolo12@gmail.com>
2024-12-10 17:32:40 +0100

    azure signalr

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddSignalR();
+builder.Services.AddSignalR().AddAzureSignalR();
 
 builder.Services.AddBlazorBootstrap();
 
